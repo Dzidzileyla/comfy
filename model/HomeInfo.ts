83ee34a10623e3a0593e332f3a9375fb9573ec25
Mateusz Kopko <kopko.mateusz@gmail.com>
2021-04-03 17:56:21 +0200

    get more information from home details

--- model/HomeInfo.ts
+++ model/HomeInfo.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 
 const webScrappingTest = async () => {
   const { data } = await axios.get(
-    'https://www.otodom.pl/pl/oferta/wroclaw-mokronos-dolny-super-oferta-ID4aFmj.html#db90a66f92'
+    'https://www.otodom.pl/pl/oferta/sprzedam-dom-prezyce-z-basenem-boisko-wroclaw-ID49QTQ.html#bf77ac3b94'
   );
 
   // fs.writeFile('sample.html', data, function (err) {
@@ -14,15 +14,98 @@ const webScrappingTest = async () => {
 
   const $ = cheerio.load(data);
 
-  const name = $('h1.css-46s0sq.edo911a18').text();
-  console.log(name);
+  const offerDescription = $('h1[data-cy=adPageAdTitle]').text();
+  console.log(offerDescription);
 
-  const generalPicture = $('picture').find('img').attr('src');
-  console.log(generalPicture);
+  const price = $('strong[aria-label=Cena]').text();
+  console.log(price);
 
-  $('div').each((i, e) => {
-    console.log($(e).attr('aria-label'));
-  });
+  const pricePerSquare = $('div[aria-label="Cena za metr kwadratowy"]').text();
+  console.log(pricePerSquare);
+
+  const imageLink = $(`img[alt="${offerDescription}"]`).attr('src');
+  console.log(imageLink);
+
+  const landArea = $('div[aria-label="Powierzchnia działki"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(landArea);
+
+  const numberOfRooms = $('div[aria-label="Liczba pokoi"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(numberOfRooms);
+
+  const buildingMaterial = $('div[aria-label="Materiał budynku"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(buildingMaterial);
+
+  const roofType = $('div[aria-label="Dach"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(roofType);
+
+  const windowType = $('div[aria-label="Okna"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(windowType);
+
+  const area = $('div[aria-label="Powierzchnia"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(area);
+
+  const typeOfBuilding = $('div[aria-label="Rodzaj zabudowy"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(typeOfBuilding);
+
+  const yearOfBuilding = $('div[aria-label="Rok budowy"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(yearOfBuilding);
+
+  const roofing = $('div[aria-label="Pokrycie dachu"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(roofing);
+
+  const location = $('div[aria-label="Położenie"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(location);
+
+  const typeOfMarket = $('div[aria-label="Rynek"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(typeOfMarket);
+
+  const numberOfFloors = $('div[aria-label="Liczba pięter"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(numberOfFloors);
+
+  const typeOfAttic = $('div[aria-label="Poddasze"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(typeOfAttic);
+
+  const state = $('div[aria-label="Stan wykończenia"]')
+    .find('div[class="css-1ytkscc ecjfvbm0"]')
+    .text();
+  console.log(state);
+
+  const description = $('div[data-cy=adPageAdDescription]').find('p').text();
+  console.log(description);
+
+  const security = $('h3[class="css-1tohpeb e9d1vc82"]').get(0);
+  console.log($(security).text());
+  console.log($(security).html());
+
+  // $(security).find('li').each((i,e) => {
+  //   console.log($(e).text());
+  // });
 
   return null;
 };
